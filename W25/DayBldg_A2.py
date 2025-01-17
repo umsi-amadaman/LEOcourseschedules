d97a8cadb6d7b211b68585f42f4b94de456841ad
umsi-amadaman <70445692+umsi-amadaman@users.noreply.github.com>
2025-01-17 12:47:34 -0500

    Update DayBldg_A2.py

--- W25/DayBldg_A2.py
+++ W25/DayBldg_A2.py
@@ -10,11 +10,14 @@ new_Bldgs = json.loads(response.text)
 
 
 
-DATA = 'https://github.com/umsi-amadaman/LEOcourseschedules/W25/raw/main/A2SchedW25.csv'
+DATA = 'https://github.com/umsi-amadaman/LEOcourseschedules/raw/main/W25/A2SchedW25.csv'
+#https://github.com/umsi-amadaman/LEOcourseschedules/blob/main/W25/A2SchedW25.csv
 
 sched = pd.read_csv(DATA)
 
 monthlydata = 'https://raw.githubusercontent.com/umsi-amadaman/LEOcourseschedules/refs/heads/main/LEOmonthly_Jan25.csv'
+
+
 monthly = pd.read_csv(monthlydata)
 
 
