d50a4363c7904e0864b707fa044245185b39ef29
umsi-amadaman <70445692+umsi-amadaman@users.noreply.github.com>
2025-05-19 14:27:00 -0400

    Update DaySubjDearborn.py

--- SS25/DaySubjDearborn.py
+++ SS25/DaySubjDearborn.py
@@ -74,7 +74,7 @@ final_df = subject_filtered_df
 #final_df = final_df
 
 #final_df = final_df.drop(columns=['Class Nbr'])
-st.write("Available columns in final_df:", list(final_df.columns))
+#st.write("Available columns in final_df:", list(final_df.columns))
 
 
 final_df = final_df[['Meeting Time Start', 'Meeting Time End','Room Code', 'Building Code', 'Primary Instructor Last Name',
